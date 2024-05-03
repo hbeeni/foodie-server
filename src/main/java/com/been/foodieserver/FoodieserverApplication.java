e20c931dec458a3894cd27ef0c55cf55b99820ad
hbeeni <hyebeen.dev@gmail.com>
2024-05-03 13:40:59 +0900

    [#2] Foodie SNS 프로젝트 생성
    1. 필요하다고 생각되는 의존성을 추가해 프로젝트 생성
    - Spring Web
    - Spring Data JPA
    - Spring Security
    - MySQL
    - H2
    - Lombok
    
    2. .gitignore 수정
    - https://www.toptal.com/developers/gitignore/ 사이트 참고

---
+++ src/main/java/com/been/foodieserver/FoodieserverApplication.java
@@ -0,0 +1,13 @@
+package com.been.foodieserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodieserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodieserverApplication.class, args);
+	}
+
+}
