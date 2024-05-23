541b90f5645c886a088fbc34c6abaa770f7d5bff
hbeeni <hyebeen.dev@gmail.com>
2024-05-23 21:24:41 +0900

    [#38] 예외 로그를 출력하는 AOP 구현

---
+++ src/main/java/com/been/foodieserver/aop/ExceptionLoggingAspect.java
@@ -0,0 +1,26 @@
+package com.been.foodieserver.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Aspect
+public class ExceptionLoggingAspect {
+
+    @Before("execution(* com.been.foodieserver.exception.handler.GlobalExceptionHandler.*(..))")
+    public void doBefore(JoinPoint joinPoint) {
+        try {
+            printLog((Exception) joinPoint.getArgs()[0]);
+        } catch (Exception e) {
+            log.error("Error occurs! [LogExceptionAspect: {}]", e.getMessage());
+        }
+    }
+
+    private static void printLog(Exception ex) {
+        log.error("Error occurs! [{}: {}]", ex.getClass().getSimpleName(), ex.getMessage());
+    }
+}
