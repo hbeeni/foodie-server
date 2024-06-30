707f28d61981b1aeeaffa03b1bb7ec8169994a8b
hbeeni <hyebeen.dev@gmail.com>
2024-07-01 06:19:57 +0900

    [#66] Redis에 데이터를 넣는 Refresh 코드 수정
    - 데이터를 페이지 단위로 불러오지 않고 한 번에 모든 데이터를 가져와서 Redis에 넣게끔 수정

--- src/main/java/com/been/foodieserver/repository/CommentRepository.java
+++ src/main/java/com/been/foodieserver/repository/CommentRepository.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @Query("select c from Comment c join fetch c.post")
-    Page<Comment> findAllWithPost(Pageable pageable);
+    List<Comment> findAllWithPost();
 
     @Query(value = "select c from Comment c " +
             "join fetch c.user u " +
--- src/main/java/com/been/foodieserver/service/RefreshService.java
+++ src/main/java/com/been/foodieserver/service/RefreshService.java
@@ -1,7 +1,6 @@
 package com.been.foodieserver.service;
 
-import com.been.foodieserver.domain.Comment;
-import com.been.foodieserver.domain.Post;
+import com.been.foodieserver.domain.User;
 import com.been.foodieserver.repository.CategoryRepository;
 import com.been.foodieserver.repository.CommentRepository;
 import com.been.foodieserver.repository.PostRepository;
@@ -12,13 +11,12 @@ import com.been.foodieserver.repository.cache.PostCacheRepository;
 import com.been.foodieserver.repository.cache.UserCacheRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -36,37 +34,32 @@ public class RefreshService {
     private final CommentCacheRepository commentCacheRepository;
 
     public void refreshUsers() {
-        userRepository.findAll().forEach(userCacheRepository::save);
+        boolean hasNext = true;
+        int i = 0;
+
+        while (hasNext) {
+            log.info("i={}", i);
+            Pageable pageable = PageRequest.of(i, 10000, Sort.by(Sort.Direction.DESC, "id"));
+            Page<User> result = userRepository.findAll(pageable);
+
+            result.getContent().forEach(userCacheRepository::save);
+            hasNext = result.hasNext();
+            i++;
+        }
+
         log.info("set users to redis complete");
     }
 
     public void refreshPosts() {
         categoryRepository.findAll().forEach(categoryCacheRepository::save);
-
-        for (int i = 0; i < 10; i++) {
-            log.info("i={}", i);
-            Pageable pageable = PageRequest.of(i, 10000, Sort.by(Sort.Direction.DESC, "id"));
-            List<Post> posts = postRepository.findAllWithUserAndCategory(pageable).getContent();
-
-            for (Post post : posts) {
-                postCacheRepository.save(post);
-            }
-        }
-
+        log.info("set posts to redis start");
+        postRepository.findAllWithUserAndCategory().forEach(postCacheRepository::save);
         log.info("set posts to redis complete");
     }
 
     public void refreshComments() {
-        for (int i = 0; i < 10; i++) {
-            log.info("i={}", i);
-            Pageable pageable = PageRequest.of(i, 10000, Sort.by(Sort.Direction.DESC, "id"));
-            List<Comment> comments = commentRepository.findAllWithPost(pageable).getContent();
-
-            for (Comment comment : comments) {
-                commentCacheRepository.saveId(comment);
-            }
-        }
-
+        log.info("set comments to redis start");
+        commentRepository.findAllWithPost().forEach(commentCacheRepository::saveId);
         log.info("set comments to redis complete");
     }
 }
